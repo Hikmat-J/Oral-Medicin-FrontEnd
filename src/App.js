89bbef7d4ed7b83201098b11e1379c4ee31bf9a8
Hikmat <Hikmatjaafer7@gmail.com>
2023-03-11 13:31:40 -0800

    edit proxy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import AddDisease from './pages/AddDisease';
 import bgImage from './assests/images/123.jpg'
 function App() {
   return (
-    <div style={{ backgroundImage:`url(${bgImage})`, backdropFilter:'blur(0.5)' , backgroundRepeat:'repeat', fontFamily: 'sans-serif' }}>
+    <div className='my-0 ' style={{ opacity:0.9, backgroundImage:`url(${bgImage})` , backgroundRepeat:'repeat', fontFamily: 'sans-serif' }}>
       <Header />
       <div className='my-2 text-center fw-light fs-2 text-secondary' style={{fontFamily:'monospace'}}>
         Add disease
