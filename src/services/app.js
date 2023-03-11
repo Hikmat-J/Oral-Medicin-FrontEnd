cf17ead89fc4394b991baf32eb29dec001db1e81
Hikmat <Hikmatjaafer7@gmail.com>
2023-03-11 14:03:31 -0800

    edit

--- src/services/app.js
+++ src/services/app.js
@@ -1,52 +1,67 @@
 import SERVICES from './index'
 
 export async function GetProperty(PropertyName, OnSuccessFunc, OnErrorFunc) {
-    await SERVICES.HTTP.Get(`/${PropertyName}/`).then(res => {
+    await SERVICES.HTTP.Get(`/api/${PropertyName}/`).then(res => {
         if (res.data !== undefined)
             if (OnSuccessFunc && typeof (OnSuccessFunc) == 'function')
                 OnSuccessFunc(res.data)
     }
     ).catch(err => {
-        if (OnErrorFunc && typeof (OnErrorFunc) == 'function')
+        if (OnErrorFunc && typeof (OnErrorFunc) == 'function'){
+            alert('Error : '+err)
             OnErrorFunc(err)
+        }
     })
 }
 
 export async function AddProperty(PropertyName, model, OnSuccessFunc, OnErrorFunc) {
-    await SERVICES.HTTP.Post(`/${PropertyName}/`, model).then(res => {
+    await SERVICES.HTTP.Post(`/api/${PropertyName}/`, model).then(res => {
         if (res.data !== undefined)
-            if (OnSuccessFunc && typeof (OnSuccessFunc) == 'function')
+            if (OnSuccessFunc && typeof (OnSuccessFunc) == 'function'){
+                alert('Property added successfully')
                 OnSuccessFunc(res.data)
+                
+            }
     }
     ).catch(err => {
-        if (OnErrorFunc && typeof (OnErrorFunc) == 'function')
+        if (OnErrorFunc && typeof (OnErrorFunc) == 'function'){
+            alert('Error : '+err)
             OnErrorFunc(err)
+        }
     })
 }
 
 export async function AddDisease(model, OnSuccessFunc, OnErrorFunc) {
-    await SERVICES.HTTP.Post('/disease/', model).then(res => {
+    await SERVICES.HTTP.Post('/api/disease/', model).then(res => {
         if (res.data !== undefined)
-            if (OnSuccessFunc && typeof (OnSuccessFunc) == 'function')
+            if (OnSuccessFunc && typeof (OnSuccessFunc) == 'function'){
+                alert('Disease added successfully')
                 OnSuccessFunc(res.data)
+            }
     }
     ).catch(err => {
-        if (OnErrorFunc && typeof (OnErrorFunc) == 'function')
+        if (OnErrorFunc && typeof (OnErrorFunc) == 'function'){
+            alert('Error : '+err)
             OnErrorFunc(err)
+        }
     })
 }
 
 export async function UploadImages(imageFile, ind, OnSuccessFunc, OnErrorFunc) {
     var image = new FormData();
     image.append(`image`, imageFile, imageFile.name)
-    await SERVICES.HTTP.Post('/images/upload', image, true).then(res => {
+    await SERVICES.HTTP.Post('/api/images/upload', image, true).then(res => {
         if (res.data !== undefined)
-            if (OnSuccessFunc && typeof (OnSuccessFunc) == 'function')
+            if (OnSuccessFunc && typeof (OnSuccessFunc) == 'function'){
+                alert('image added successfully')
                 OnSuccessFunc(res.data)
+            }
     }
     ).catch(err => {
-        if (OnErrorFunc && typeof (OnErrorFunc) == 'function')
+        if (OnErrorFunc && typeof (OnErrorFunc) == 'function'){
+            alert('Error : '+err)
             OnErrorFunc(err)
+        }
     })
 }
 
